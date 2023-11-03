4876cef48d916655f76319282bcb868ca79bc639
aleksandr0060 <149719781+aleksandr0060@users.noreply.github.com>
2023-11-03 22:42:49 +0300

    Add files via upload

---
+++ tg.php
@@ -0,0 +1,38 @@
+
+<?php
+
+$name = $_POST['userName'];
+$versiya = $_POST['userName1'];
+$osnisp = $_POST['userName2'];
+$priych = $_POST['userName3'];
+$raznisp = $_POST['musician'];
+$janr = $_POST['userName5'];
+$fio = $_POST['userName6'];
+$prod = $_POST['userName7'];
+$kontakt = $_POST['userName8'];
+$token = "6868567195:AAF1zd0Ta35VY0_1oWHETQQb9HL3TMJP_cs";
+$chat_id = "-1002038908582";
+$arr = array(
+  'Название релиза:' => $name,
+  'Версия/Подзаголовок:' => $versiya,
+  'Основной исполнитель:' => $osnisp,
+  'При участии:' => $priych,
+  'Разные исполнители/Сборник' => $raznisp,
+  'Жанр:' => $janr,
+  'Полное ФИО одного из артистов:' => $fio,
+  'prod.by и т.д.(Если не нужно указывать, то оставьте поле пустым):' => $prod,
+  'Контакт для связи (вк/тг):' => $kontakt
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
